6b921b77d6b65d11dac883f278d8c11b13c9220b
peymantahghighi <tahghighipeyman@gmail.com>
2022-05-14 20:02:30 -0400

    Unimportant changes

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu";
 LEARNING_RATE = 1e-5
-BATCH_SIZE = 2
+BATCH_SIZE = 1
 NUM_WORKERS = 1
 IMAGE_SIZE = 1024
 NUM_EPOCHS = 40
